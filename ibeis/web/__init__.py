86aacf5bc2f32dfa7fea63e86b158e8683e8eb38
joncrall <erotemic@gmail.com>
2015-11-25 10:19:16 -0500

    updated docs

--- ibeis/web/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# flake8: noqa
-from __future__ import absolute_import, division, print_function
-import utool
-(print, print_, printDBG, rrr, profile) = utool.inject(__name__, '[web]', DEBUG=False)
-
-from ibeis.web import app
-from ibeis.web import appfuncs
