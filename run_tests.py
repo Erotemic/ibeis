e9510d50c9aabbde05d9117a19c54087c83c5498
joncrall <erotemic@gmail.com>
2023-01-28 22:09:36 -0500

    Update xcookie structure

--- run_tests.py
+++ run_tests.py
@@ -3,10 +3,10 @@ if __name__ == '__main__':
     import pytest
     import sys
     package_name = 'ibeis'
-    mod_dpath = package_name
+    mod_dpath = 'ibeis'
     test_dpath = 'tests'
     pytest_args = [
-        '--cov-config', '.coveragerc',
+        '--cov-config', 'pyproject.toml',
         '--cov-report', 'html',
         '--cov-report', 'term',
         '--xdoctest',
