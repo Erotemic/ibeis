412196a4c61058a52dd470dfa530998b42de0f5c
joncrall <erotemic@gmail.com>
2026-02-04 21:25:15 -0500

    wip

--- dev/_installers/build_installer.sh
+++ dev/_installers/build_installer.sh
@@ -1,3 +1,13 @@
 #cd ~/code/ibeis
 python -m pip install -U PyInstaller
 python -m PyInstaller -y dev/_installers/pyinstaller-ibeis.spec
+
+# Need INNO on windows
+winget search innosetup
+
+Set-Alias iscc
+iscc "dev\_installers\ibeis.iss"
+
+#"$env:LOCALAPPDATA\Programs\Inno Setup 6\ISCC.exe" "dev\_installers\win_installer_script.iss"
+& "$env:LOCALAPPDATA\Programs\Inno Setup 6\ISCC.exe" ".\dev\_installers\win_installer_script.iss"
+
